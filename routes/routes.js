876a3cfff711ed5b5eacf16f12a9125af1df19ff
MohitGupta121 <guptamm.22@gmail.com>
2022-09-05 23:42:39 +0530

    folder stru

--- routes/routes.js
+++
@@ -1,6 +0,0 @@
-const express =require("express")
-const app=express()
-const port=process.env.port || 5000
-
-// Listen on enviroment port or 5000
-app.listen(port, () => console.log(`Listen on port ${port}`))
