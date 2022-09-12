894ba77ef5044e21712893db7b7fc93c6b14f1cf
Mohit Gupta <guptamm.22@gmail.com>
2022-09-12 17:23:47 +0530

    Images: remove static image

--- sequelize/app.js
+++ sequelize/app.js
@@ -8,9 +8,5 @@ app.use(express.json());
 // Routers
 app.use("/", router);
 
-//static Images Folder
-app.use('/Images', express.static('./Images'))
-
-
 const port = process.env.port || 8000;
 app.listen(port, () => console.log(`Listen on port ${port}`));
